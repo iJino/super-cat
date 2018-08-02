fc63241e59f3127d1f224ecf8717803f2a4ca2b2
superCatNotSB <1023402488@qq.com>
2018-08-02 15:53:25 +0800

    update

--- src/main/java/com/liangjinhai/supercat/sys/service/impl/UserServiceImpl.java
+++ src/main/java/com/liangjinhai/supercat/sys/service/impl/UserServiceImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 
-@Service("basicUserService")
+@Service("userService")
 public class UserServiceImpl implements UserService {
 
     @Autowired
